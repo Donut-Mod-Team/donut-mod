a6d8a0bf8dfdc1443e139d99fd5c0d2e5cdb9ea5
Krasen Parvanov <krasenparvanov@gmail.com>
2021-09-15 11:16:17 +0200

    DMT-6 Add basic donut visualization
    Implement basic visualization of data in donut-chart using the template from tibco-mods
    
    Co-Authored-By: Clementine Jensen <gusjensecl@student.gu.se>
    Co-Authored-By: Eemil Jeskanen <jeskanen.eemil@gmail.com>
    Co-Authored-By: Chrysostomos Tsagkidis <62544931+chrytsa@users.noreply.github.com>

---
+++ src/donutChart.js
@@ -0,0 +1,64 @@
+/**
+ *
+ * @param {Spotfire.Size} size
+ * @param {any[]} data
+ * @param {Spotfire.Mod} mod
+ */
+import * as d3 from "d3"
+
+export function donutChart(size, data, mod) {
+    const width = size.width - 40;
+    const height = size.height - 40;
+    const radius = Math.min(width, height) / 2 - 40;
+
+    d3.select("#mod-container svg").attr("width", width).attr("height", height);
+    const g = d3.select("#mod-container svg g").attr("transform", `translate(${width / 2}, ${height / 2})`);
+
+    const pie = d3.pie().value((d) => d.value);
+
+    const arc = d3
+        .arc()
+        .padAngle(0.1 / data.length)
+        .innerRadius(radius * 0.5)
+        .outerRadius(radius);
+
+    // Join new data
+    const path = g.selectAll("path").data(pie(data), (d) => {
+        return d.data.id;
+    });
+
+    let newPaths = path
+        .enter()
+        .append("path")
+        .on("click", function (d) {
+            d.data.mark();
+        })
+        .on("mouseenter", function (d) {
+            mod.controls.tooltip.show(d.data.tooltip());
+        })
+        .on("mouseleave", function (d) {
+            mod.controls.tooltip.hide();
+        })
+        .attr("fill", (d) => "transparent");
+
+    path.merge(newPaths)
+        .transition()
+        .duration(300)
+        .attr("fill", (d) => d.data.color)
+        .attrTween("d", tweenArc)
+        .attr("stroke", "none");
+
+    function tweenArc(elem) {
+        let prevValue = this.__prev || {};
+        let newValue = elem;
+        this.__prev = elem;
+
+        var i = d3.interpolate(prevValue, newValue);
+
+        return function (value) {
+            return arc(i(value));
+        };
+    }
+
+    path.exit().transition().duration(300).attr("fill", "transparent").remove();
+}
--- src/main.js
+++ src/main.js
@@ -5,21 +5,25 @@
  */
 
 //@ts-check - Get type warnings from the TypeScript language server. Remove if not wanted.
-
+import { donutChart } from "./donutChart"
 /**
  * Get access to the Spotfire Mod API by providing a callback to the initialize method.
  * @param {Spotfire.Mod} mod - mod api
  */
- Spotfire.initialize(async (mod) => {
-    /**
-     * Create the read function.
-     */
-    const reader = mod.createReader(mod.visualization.data(), mod.windowSize(), mod.property("myProperty"));
+const Spotfire = window.Spotfire;
 
+Spotfire.initialize(async (mod) => {
     /**
      * Store the context.
      */
     const context = mod.getRenderContext();
+
+    /**
+     * Create the read function.
+     */
+    const reader = mod.createReader(mod.visualization.data(), mod.windowSize());
+
+
 
     /**
      * Initiate the read loop
@@ -27,47 +31,126 @@
     reader.subscribe(render);
 
     /**
+     * Render the visualization
      * @param {Spotfire.DataView} dataView
-     * @param {Spotfire.Size} windowSize
-     * @param {Spotfire.ModProperty<string>} prop
+     * @param {Spotfire.Size} size
      */
-    async function render(dataView, windowSize, prop) {
+    async function render(dataView, size) {
+
         /**
-         * Check the data view for errors
+         * Check for any errors.
          */
         let errors = await dataView.getErrors();
         if (errors.length > 0) {
-            // Showing an error overlay will hide the mod iframe.
-            // Clear the mod content here to avoid flickering effect of
-            // an old configuration when next valid data view is received.
-            mod.controls.errorOverlay.show(errors);
-            return;
-        }
-        mod.controls.errorOverlay.hide();
-
-        /**
-         * Get the hierarchy of the categorical X-axis.
-         */
-        const xHierarchy = await dataView.hierarchy("X");
-        const xRoot = await xHierarchy.root();
-
-        if (xRoot == null) {
-            // User interaction caused the data view to expire.
-            // Don't clear the mod content here to avoid flickering.
+            mod.controls.errorOverlay.show(errors, "dataView");
+            // TODO clear DOM
             return;
         }
 
-        /**
-         * Print out to document
-         */
-        const container = document.querySelector("#mod-container");
-        container.textContent = `windowSize: ${windowSize.width}x${windowSize.height}\r\n`;
-        container.textContent += `should render: ${xRoot.rows().length} rows\r\n`;
-        container.textContent += `${prop.name}: ${prop.value()}`;
+        mod.controls.errorOverlay.hide("dataView");
 
-        /**
-         * Signal that the mod is ready for export.
-         */
+        // Get the leaf nodes for the x hierarchy. We will iterate over them to
+        // render the bars.
+        let colorHierarchy = await dataView.hierarchy("Color");
+        let colorRoot = await colorHierarchy.root();
+        if (colorRoot == null) {
+            // Return and wait for next call to render when reading data was aborted.
+            // Last rendered data view is still valid from a users perspective since
+            // a document modification was made during a progress indication.
+            return;
+        }
+
+        let dataViewYAxis = await dataView.continuousAxis("Y");
+        if (dataViewYAxis == null) {
+            mod.controls.errorOverlay.show("No data on y axis.", "y");
+            return;
+        } else {
+            mod.controls.errorOverlay.hide("y");
+        }
+
+        // Hide tooltip
+        mod.controls.tooltip.hide();
+
+        let colorLeaves = colorRoot.leaves();
+
+        let data = colorLeaves.map(leaf => {
+
+            let rows = leaf.rows();
+            return {
+                color: rows.length ? rows[0].color().hexCode : "transparent",
+                value: sumValue(rows, "Y"),
+                id: leaf.key,
+                mark: () => leaf.mark(),
+                tooltip: () => {
+                    return leaf.formattedValue() + " " + sumValue(rows, "Y");
+                }
+            }
+        })
+
+        donutChart(size, data, mod);
+
         context.signalRenderComplete();
     }
 });
+
+/**
+ * Calculate the maximum value from a hierarchy. If split bars is enabled, the single maximum value from all rows will be used.
+ * @param {Spotfire.DataViewHierarchyNode[]} xLeaves
+ * @param {Spotfire.ModProperty<boolean>} splitBars
+ * @param {number} categoricalColorCount
+ */
+function calculateMaxYValue(xLeaves, splitBars, categoricalColorCount) {
+    let maxYValue = 0;
+    if (splitBars.value() && categoricalColorCount > 0) {
+        xLeaves.forEach((node) => {
+            maxYValue = Math.max(maxValue(node.rows(), "Y"), maxYValue);
+        });
+    } else {
+        xLeaves.forEach((node) => {
+            let sum = sumValue(node.rows(), "Y");
+            maxYValue = Math.max(maxYValue, sum);
+        });
+    }
+
+    return maxYValue;
+}
+
+/**
+ * Calculate the total value for an axis from a set of rows. Null values are treated a 0.
+ * @param {Spotfire.DataViewRow[]} rows Rows to calculate the total value from
+ * @param {string} axis Name of Axis to use to calculate the value.
+ */
+function sumValue(rows, axis) {
+    return rows.reduce((p, c) => +c.continuous(axis).value() + p, 0);
+}
+
+/**
+ * Calculate the max value for an axis from a set of rows. Null values are treated a 0.
+ * @param {Spotfire.DataViewRow[]} rows Rows to calculate the max value from
+ * @param {string} axis Name of Axis to use to calculate the value.
+ */
+function maxValue(rows, axis) {
+    return rows.reduce((p, c) => Math.max(+c.continuous(axis).value(), p), 0);
+}
+
+/** @returns {HTMLElement} */
+function findElem(selector) {
+    return document.querySelector(selector);
+}
+
+/**
+ * Create a div element.
+ * @param {string} className class name of the div element.
+ * @param {string | HTMLElement} [content] Content inside the div
+ */
+function createDiv(className, content) {
+    let elem = document.createElement("div");
+    elem.classList.add(className);
+    if (typeof content === "string") {
+        elem.appendChild(document.createTextNode(content));
+    } else if (content) {
+        elem.appendChild(content);
+    }
+
+    return elem;
+}
