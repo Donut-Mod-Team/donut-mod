4a2f9f6e6060e49b887bbb1755336db8b97ee641
chrytsa <62544931+chrytsa@users.noreply.github.com>
2021-09-28 12:57:38 +0200

    WIP
    Co-Authored-By: Eemil Jeskanen <jeskanen.eemil@gmail.com>

--- src/renderer.js
+++ src/renderer.js
@@ -1,6 +1,6 @@
 import * as d3 from "d3";
 /**
- * @param {donutState} donutState
+ * @param {object} donutState
  */
 export async function render(donutState) {
     // Added a constant to remove the magic numbers within the width, height and radius calculations.
@@ -14,7 +14,7 @@ export async function render(donutState) {
 
     d3.select("#mod-container svg").attr("width", width).attr("height", height);
 
-    const svg = d3.select("#mod-container svg g").attr("transform", `translate(${width / 2}, ${height / 2})`);
+    const svg = d3.select("#mod-container").select("svg#svg").attr("transform", `translate(${width / 2}, ${height / 2})`);
 
     const pie = d3.pie().value((d) => d.value);
 
@@ -25,13 +25,14 @@ export async function render(donutState) {
         .outerRadius(radius);
 
     // Join new data
-    const sectors = svg.selectAll("path").data(pie(donutState.data), (d) => {
+    const sectors = svg.select("g#sectors").selectAll("path").data(pie(donutState.data), (d) => {
         return d.data.id;
     });
 
     let newSectors = sectors
         .enter()
-        .append("path")
+        .append("svg:path")
+        .attr("class", "sector")
         .on("click", function (d) {
             d.data.mark();
         })
@@ -45,11 +46,41 @@ export async function render(donutState) {
 
     sectors
         .merge(newSectors)
+        .attr("class", "sector")
         .transition()
         .duration(animationDuration)
+        .attr("value", (d) => (d.data.value / donutState.sumOfValues * 100).toFixed(1))
         .attr("fill", (d) => d.data.color)
         .attrTween("d", tweenArc)
         .attr("stroke", "none");
+
+    sectors.exit().transition().duration(animationDuration).attr("fill", "transparent").remove();
+
+    svg.select("g#labels")
+        .attr("pointer-events", "none")
+        .attr("text-anchor", "middle")
+        .selectAll("text")
+        .data(pie(donutState.data), (d) => `label-${d.data.id}`)
+        .join(
+            (enter) => {
+                return enter
+                    .append("text")
+                    .style("opacity", 0)
+                    .attr("dy", "0.35em")
+                    .attr("font-size", 40)
+                    .attr("fill", "transparent")
+                    .text((d) => (d.data.value / donutState.sumOfValues * 100).toFixed(1))
+                    .call((enter) =>
+                        enter
+                            .transition("add labels")
+                            .duration(animationDuration)
+                            .style("opacity", 1)
+                            .attrTween("transform", tweenTransform)
+                    );
+            },
+            (exit) => exit.transition("remove labels").duration(animationDuration).style("opacity", 0).remove()
+        );
+
 
     function tweenArc(elem) {
         let prevValue = this.__prev || {};
@@ -63,7 +94,25 @@ export async function render(donutState) {
         };
     }
 
-    sectors.exit().transition().duration(animationDuration).attr("fill", "transparent").remove();
+    function tweenTransform(data) {
+        let prevValue = this.__prev ? getTransformData(this.__prev) : {};
+        let newValue = getTransformData(data);
+        this.__prev = newValue;
+
+        var i = d3.interpolate(prevValue, newValue);
+
+        return function (value) {
+            var { x, y } = labelPosition(i(value));
+            return `rotate(${x - 90}) translate(${y},0) rotate(${Math.sin((Math.PI * x) / 180) >= 0 ? 0 : 180})`;
+        };
+    }
+
+    function getTransformData(data) {
+        // d3.interpolate should not try to interpolate other properties
+        return (({ value, x0, x1, y0, y1 }) => ({ value, x0, x1, y0, y1 }))(data);
+    }
+
+
 
     donutState.context.signalRenderComplete();
 }
