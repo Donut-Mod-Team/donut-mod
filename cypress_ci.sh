71ebec73dad66661907fb566fd36212638c8486d
chrytsa <62544931+chrytsa@users.noreply.github.com>
2021-11-03 15:36:25 +0100

    DMT-79 Print directories in order to debug the issue with the CI
    Co-Authored-By: Krasen Parvanov <krasenparvanov@gmail.com>

--- cypress_ci.sh
+++ cypress_ci.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+pwd
+whoami
+ls -l dist
+echo "===================================================="
+echo "\n"
+ls -l
+echo "===================================================="
+echo "\n"
 npm start & npm run server &
